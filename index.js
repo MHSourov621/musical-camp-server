5947f81c762eb3265d6a7dbf4ee84e7ff3c15441
MHSourov621 <mhsourov621@gmail.com>
2023-06-08 20:15:07 +0600

    user post api added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -8,14 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// DB_USER=musicalBoss
-// DB_PASS=1jVAuk1ySSJA1XeV
 
-
-
-const uri = `mongodb+srv://musicalBoss:1jVAuk1ySSJA1XeV@cluster0.tsr81r8.mongodb.net/?retryWrites=true&w=majority`;
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tsr81r8.mongodb.net/?retryWrites=true&w=majority`;
-console.log(process.env.DB_USER);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tsr81r8.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,6 +25,24 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const usersCollection = client.db('misicalCamp').collection('users');
+        const classesCollection = client.db('misicalCamp').collection('classes');
+
+        //users api
+
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+            const query = {email: user.email};
+            const existingUser = await usersCollection.findOne(query);
+            if(existingUser){
+                return res.send('user already existing')
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
