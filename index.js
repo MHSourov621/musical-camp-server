e138bc13d9b5fedc4c3f9438015da7b85f5c3284
MHSourov621 <mhsourov621@gmail.com>
2023-06-10 02:30:09 +0600

    selected added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -8,6 +9,22 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESs_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tsr81r8.mongodb.net/?retryWrites=true&w=majority`;
@@ -29,30 +46,38 @@ async function run() {
 
         const usersCollection = client.db('misicalCamp').collection('users');
         const classesCollection = client.db('misicalCamp').collection('classes');
+        const selectedCollection = client.db('misicalCamp').collection('selected');
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
 
         //users api
 
-        app.get('/users', async(req, res) => {
+        app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
 
-        app.post('/users', async(req, res) => {
+        app.post('/users', async (req, res) => {
             const user = req.body;
-            const query = {email: user.email};
+            const query = { email: user.email };
             const existingUser = await usersCollection.findOne(query);
-            if(existingUser){
+            if (existingUser) {
                 return res.send('user already existing')
             }
             const result = await usersCollection.insertOne(user);
             res.send(result)
         })
 
-        app.patch('/users/admin/:id', async(req, res) => {
+        app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id : new ObjectId(id)};
+            const filter = { _id: new ObjectId(id) };
             const updateDoc = {
-                $set : {
+                $set: {
                     role: 'admin'
                 },
             };
@@ -60,11 +85,11 @@ async function run() {
             res.send(result);
         })
 
-        app.patch('/users/instructor/:id', async(req, res) => {
+        app.patch('/users/instructor/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id : new ObjectId(id)};
+            const filter = { _id: new ObjectId(id) };
             const updateDoc = {
-                $set : {
+                $set: {
                     role: 'instructor'
                 },
             };
@@ -72,28 +97,43 @@ async function run() {
             res.send(result);
         })
 
-        app.delete('/users/:id', async(req, res) => {
+        app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id : new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await usersCollection.deleteOne(query);
             res.send(result)
         })
 
         //instructor api
 
-        app.get('/instructors', async(req, res) => {
-            const query = {role : 'instructor'};
-            const result =  await usersCollection.find(query).toArray();
+        app.get('/instructors', async (req, res) => {
+            const query = { role: 'instructor' };
+            const result = await usersCollection.find(query).toArray();
             res.send(result);
         })
 
 
         //classes api
 
-        app.get('/classes', async(req, res) => {
-            const query = {status : 'approved'}
-            const result = await classesCollection.find(query).sort({available_seats : 1}).toArray();
+        app.get('/classes', async (req, res) => {
+            const query = { status: 'approved' }
+            const result = await classesCollection.find(query).sort({ available_seats: 1 }).toArray();
             res.send(result)
+        })
+
+        // selected api
+
+        app.get('/selected/:email', async(req, res) => {
+            const email = req.params.email;
+            const query = {email: email};
+            const result = await selectedCollection.find(query).toArray();
+            res.send(result); 
+        })
+
+        app.post('/selected', async(req, res) => {
+            const course = req.body;
+            const result = await selectedCollection.insertOne(course);
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
